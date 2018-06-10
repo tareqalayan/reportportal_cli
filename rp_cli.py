6e2bf9d41b2ee650e0f0232861c985d6f8ac565e
Tareq Alayan <tareq.alayan@gmail.com>
2018-06-10 12:21:16 +0300

    refactor and some fixes

--- rp_cli.py
+++ rp_cli.py
@@ -4,55 +4,66 @@ import logging
 import yaml
 import requests
 import json
-import junitparser
-from time import time
+import time
 import traceback
-from reportportal_client import ReportPortalServiceAsync
 import zipfile
 import os
-# from ipdb import set_trace
+
+import junitparser
+from reportportal_client import ReportPortalServiceAsync
 
 
-logger = logging.getLogger("reportportal_cli.py")
+logger = logging.getLogger("rp_cli.py")
 
 
-def zipdir(path, ziph):
-    # ziph is zipfile handle
+def zip_dir(path, zip_file):
     for root, dirs, files in os.walk(path):
-        for file in files:
-            ziph.write(os.path.join(root, file))
+        for f in files:
+            zip_file.write(os.path.join(root, f))
 
 
 def timestamp():
-    return str(int(time() * 1000))
+    return str(int(time.time() * 1000))
 
 
-def my_error_handler(exc_info):
-    """
-    This callback function will be called by async service client when error occurs.
-    Return True if error is not critical and you want to continue work.
-    :param exc_info: result of sys.exc_info() -> (type, value, traceback)
-    :return:
-    """
-    print("Error occurred: {}".format(exc_info[1]))
-    traceback.print_exception(*exc_info)
+def init_logger(level):
+    formatter = "%(asctime)s:%(name)s:%(levelname)s:%(threadName)s:%(message)s"
+    logging.basicConfig(format=formatter, level=level)
 
 
-def extract_error_msg_from_xunit(s, first, last):
-    try:
-        start = s.index(first) + len(first)
-        end = s.index(last, start)
-        return s[start:end]
-    except ValueError:
-        return ""
+class MyCustomizations:
+    def __init__(self):
+        pass
 
+    @staticmethod
+    def my_error_handler(exc_info):
+        """
+        This callback function will be called by async service client when error occurs.
+        Return True if error is not critical and you want to continue work.
 
-def get_logs_per_test_path(name):
-    return '/'.join(name.split('.')[1:])
+        Args:
+            exc_info: result of sys.exc_info() -> (type, value, traceback)
+
+        """
+        logger.error("Error occurred: {}".format(exc_info[1]))
+        traceback.print_exception(*exc_info)
+
+    @staticmethod
+    def extract_error_msg_from_xunit(case):
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_logs_per_test_path(case):
+        raise NotImplementedError()
+
+    @staticmethod
+    def get_tags(case):
+        raise NotImplementedError()
+# END: Class MyCustomization
 
 
 class RpManager:
-    def __init__(self, config, error_msg_extractor=None):
+    def __init__(self, config, strategy=MyCustomizations):
         self.url = config.get('rp_endpoint')
         self.uuid = config.get('rp_uuid')
         self.project = config.get('rp_project')
@@ -75,12 +86,13 @@ class RpManager:
         )
         self.xunit_feed = config.get('xunit_feed')
         self.launch_name = config.get('launch_name', 'rp_cli-launch')
+        self.strategy = strategy
         self.service = ReportPortalServiceAsync(
-            endpoint=self.url, project=self.project, token=self.uuid, error_handler=my_error_handler
+            endpoint=self.url, project=self.project, token=self.uuid, error_handler=self.strategy.my_error_handler
         )
         self.test_logs = config.get('test_logs')
         self.zipped = config.get('zipped')
-        self.extract_err_msg = error_msg_extractor
+        self.extract_err_msg = self.strategy.extract_error_msg_from_xunit
 
     @staticmethod
     def _check_return_code(req):
@@ -140,22 +152,20 @@ class RpManager:
         self.service.terminate()
 
     def upload_test_case_attachments(self, path):
-        # set_trace()
         for root, dirs, files in os.walk(path):
             for log_file in files:
                 with open(root+"/"+log_file, "rb") as fh:
                     attachment = {
                         "name": log_file,
                         "data": fh.read(),
-                        "mime": "text/plain"  # "application/octet-stream"
+                        "mime": "text/plain"
                     }
                     self.service.log(timestamp(), log_file, "INFO", attachment)
 
     def upload_zipped_test_case_attachments(self, zip_file_name, path):
-        # set_trace()
-        zipf = zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED)
-        zipdir(self.test_logs + '/' + path, zipf)
-        zipf.close()
+        with zipfile.ZipFile(zip_file_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+            zip_dir(self.test_logs + '/' + path, zip_file)
+
         with open(zip_file_name, "rb") as fh:
             attachment = {
                 "name": os.path.basename(zip_file_name),
@@ -170,10 +180,9 @@ class RpManager:
         xml = junitparser.JUnitXml.fromfile(self.xunit_feed)
         for case in xml:
 
-            name = case.classname + '.' + case.name
-            description = case.name + " time:" + str(case.time)
-            tags = list()
-            tags.append(case.classname.split('.')[1])
+            name = "{class_name}.{tc_name}".format(class_name=case.classname, tc_name=case.name)
+            description = "{tc_name} time: {case_time}".format(tc_name=case.name, case_time=case.time)
+            tags = self.strategy.get_tags(case)
 
             self.service.start_test_item(
                 name=name[:255],
@@ -191,11 +200,10 @@ class RpManager:
 
             if not case.result:
                 status = 'PASSED'
-            if isinstance(case.result, junitparser.junitparser.Skipped):
+            elif isinstance(case.result, junitparser.junitparser.Skipped):
                 status = 'SKIPPED'
-            if isinstance(case.result, junitparser.junitparser.Error):
+            elif isinstance(case.result, junitparser.junitparser.Error):
                 status = 'FAILED'
-                error = case.result.tostring()
                 self.service.log(
                     time=timestamp(),
                     message=case.result.message,
@@ -203,30 +211,24 @@ class RpManager:
                 )
                 self.service.log(
                     time=timestamp(),
-                    message=extract_error_msg_from_xunit(error, ">", "</error>"),
+                    message=self.strategy.extract_error_msg_from_xunit(case),
                     level="ERROR"
                 )
                 if self.test_logs:
                     if self.zipped:
                         # zip logs per test and upload zip file
-                        path_to_logs_per_test = '/'.join(name.split('.')[1:])
-                        self.upload_zipped_test_case_attachments(case.name+".zip", path_to_logs_per_test)
-
+                        path_to_logs_per_test = self.strategy.get_logs_per_test_path(case)
+                        self.upload_zipped_test_case_attachments("{0}.zip".format(case.name), path_to_logs_per_test)
                     else:
-                        # set_trace()
-                        # upload logs per tests one by one and not zip them
-                        path_to_logs_per_test = '/'.join(name.split('.')[1:])
-                        self.upload_test_case_attachments(self.test_logs+'/'+path_to_logs_per_test)
+                        # upload logs per tests one by one and do not zip them
+                        path_to_logs_per_test = self.strategy.get_logs_per_test_path(case)
+                        self.upload_test_case_attachments("{0}/{1}".format(self.test_logs, path_to_logs_per_test))
 
-            self.service.finish_test_item(end_time=timestamp(), status=status)  # issue=issue)
+            self.service.finish_test_item(end_time=timestamp(), status=status)
 
         # Finish launch.
         self._end_launch()
-
-
-def init_logger(level):
-    fmt = "%(asctime)s:%(name)s:%(levelname)s:%(threadName)s:%(message)s"
-    logging.basicConfig(format=fmt, level=level)
+# End class RpManager
 
 
 def parse_configuration_file(config):
@@ -235,19 +237,20 @@ def parse_configuration_file(config):
 
     Returns: dictionary containing the configuration file data
     """
+
     try:
         with open(config, 'r') as stream:
-            config_data = yaml.load(stream)
+            conf_data = yaml.load(stream)
     except (OSError, IOError) as error:
         logger.error("Failed when opening config file. Error: %s", error)
         sys.exit(1)
 
     # Check configuration file:
-    if not all(key in config_data for key in ['rp_endpoint', 'rp_uuid', 'rp_project']):
+    if not all(key in conf_data for key in ['rp_endpoint', 'rp_uuid', 'rp_project']):
         logger.error('Configuration file missing one of: rp_endpoint, rp_uuid or rp_project')
         sys.exit(1)
 
-    return config_data
+    return conf_data
 
 
 def parser():
@@ -256,52 +259,55 @@ def parser():
 
     Returns: A dictionary containing parsed arguments
     """
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
+    rp_parser = argparse.ArgumentParser()
+    rp_parser.add_argument(
         "--config", type=str, required=True,
         help="Configuration file path",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--upload_xunit", type=str, required=False,
         help="launch_name.zip: zip file contains the xunit.xml",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--launch_name", type=str, required=False,
         help="Description of the launch",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--launch_description", type=str, required=False,
         help="Description of the launch",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--launch_tags", type=str, required=False,
         help="Tags for that launch",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--xunit_feed", type=str, required=False,
         help="Parse xunit and feed data to report portal",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--test_logs", type=str, required=False,
         help="Path to folder where all logs per tests are located.",
     )
-    parser.add_argument(
+    rp_parser.add_argument(
         "--zipped", action='store_true',
         help="True to upload the logs zipped to save time and traffic",
     )
 
-    return parser
+    return rp_parser
 
 
 if __name__ == "__main__":
+
     init_logger(logging.DEBUG)
 
-    parser = parser()
-    args = parser.parse_args()
+    rp_parser = parser()
+    args = rp_parser.parse_args()
 
     config_data = parse_configuration_file(args.config)
     config_data.update(args.__dict__)
-    rp = RpManager(config_data, extract_error_msg_from_xunit)
+
+    strategy = MyCustomizations()
+    rp = RpManager(config_data, strategy)
 
     if args.upload_xunit:
         launch_id = rp.import_results()
@@ -311,4 +317,4 @@ if __name__ == "__main__":
         rp.feed_results()
     else:
         logger.error("Bad command see usage:")
-        parser.print_help()
+        rp_parser.print_help()
