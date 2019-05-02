bac3e4b08ca382d2917e55b434fb35fd8025098a
Tareq Alayan <talayan@redhat.com>
2019-05-02 16:22:47 +0300

    ignore if skipped nodes return none

--- rp_cli.py
+++ rp_cli.py
@@ -470,7 +470,8 @@ class RpManager:
             if 'skipped' in case.keys():
                 issue = {"issue_type": "NOT_ISSUE"}  # this will cause skipped test to not be "To Investigate"
                 status = 'SKIPPED'
-                self._log_message_to_rp_console(case.get('skipped').get('@message'), "DEBUG")
+                if case.get('skipped'):
+                    self._log_message_to_rp_console(case.get('skipped').get('@message'), "DEBUG")
             elif case.get('failure') or case.get('error'):  # Error or failed cases
                 status = 'FAILED'
                 self._process_failed_case(case)
